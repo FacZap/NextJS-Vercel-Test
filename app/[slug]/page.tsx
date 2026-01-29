6a3fad6eacaa3acb5e64338d00d76955e28c7dd2
FacundoZapata <zapatafacundo17@gamil.com>
2026-01-29 16:34:53 -0300

    failed Vercel launch

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { BlogPost } from "@/types/blog-post";
+import { BlogPost } from "@/types/blog-posts";
 
 async function getBlogPost(slug: string): Promise<BlogPost> {
   const response = await fetch(`http://localhost:3000/api/v1/posts/${slug}`);
--- app/api/v1/posts/[slug]/route.ts
+++ app/api/v1/posts/[slug]/route.ts
@@ -1,16 +1,38 @@
+import { NextRequest } from "next/server";
 import { BLOG_POSTS } from "@/data/blog-posts";
 
 // GET "localhost:3000/api/v1/posts/linkin-park"
 
 export async function GET(
   request: NextRequest,
-  context: { params: { slug: string } },
+  { params }: { params: Promise<{ slug: string }> },
 ): Promise<Response> {
-  const params = await context.params;
-  const slug = params.slug;
-  for (let i = 0; i < BLOG_POSTS.length; i++) {
-    if (slug == BLOG_POSTS[i].slug) {
-      return new Response(JSON.stringify(BLOG_POSTS[i]));
-    }
+  // En Next 16 params es un Promise
+  const { slug } = await params;
+
+  const post = BLOG_POSTS.find((p) => p.slug === slug);
+
+  if (!post) {
+    return new Response("Post not found", { status: 404 });
   }
+
+  return new Response(JSON.stringify(post), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 }
+
+// export async function GET(
+//   request: NextRequest,
+//   context: { params: <{ slug: string }> },
+// ): Promise<Response> {
+//   const params = await context.params;
+//   const slug = params.slug;
+// for (let i = 0; i < BLOG_POSTS.length; i++) {
+//   if (slug == BLOG_POSTS[i].slug) {
+//     return new Response(JSON.stringify(BLOG_POSTS[i]));
+//   }
+// }
+//}
